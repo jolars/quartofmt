dd59d631f2208da6ecbd365e2fdbcde2fb6d2c5f
Johan Larsson <jolars@posteo.com>
2025-09-13 22:22:16 +0200

    chore: improve formatting of tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -483,13 +483,13 @@ fn lexer_math_block_tokens() {
         kinds,
         vec![
             SyntaxKind::BlockMathMarker, // $$
-            SyntaxKind::NEWLINE,
-            SyntaxKind::TEXT, // f(x) = x^2
-            SyntaxKind::NEWLINE,
+            SyntaxKind::NEWLINE,         //
+            SyntaxKind::TEXT,            // f(x) = x^2
+            SyntaxKind::NEWLINE,         //
             SyntaxKind::BlockMathMarker, // $$
-            SyntaxKind::WHITESPACE,
-            SyntaxKind::Label, // {#eq:foobar}
-            SyntaxKind::NEWLINE,
+            SyntaxKind::WHITESPACE,      //
+            SyntaxKind::Label,           // {#eq:foobar}
+            SyntaxKind::NEWLINE,         //
         ]
     );
 }
@@ -502,17 +502,17 @@ fn lexer_inline_math_tokens() {
     assert_eq!(
         kinds,
         vec![
-            SyntaxKind::TEXT, // This
-            SyntaxKind::WHITESPACE,
-            SyntaxKind::TEXT, // is
-            SyntaxKind::WHITESPACE,
+            SyntaxKind::TEXT,             // This
+            SyntaxKind::WHITESPACE,       //
+            SyntaxKind::TEXT,             // is
+            SyntaxKind::WHITESPACE,       //
             SyntaxKind::InlineMathMarker, // $
             SyntaxKind::TEXT,             // x^2
             SyntaxKind::InlineMathMarker, // $
-            SyntaxKind::WHITESPACE,
-            SyntaxKind::TEXT, // inline
-            SyntaxKind::WHITESPACE,
-            SyntaxKind::TEXT, // math.
+            SyntaxKind::WHITESPACE,       //
+            SyntaxKind::TEXT,             // inline
+            SyntaxKind::WHITESPACE,       //
+            SyntaxKind::TEXT,             // math.
         ]
     );
 }
